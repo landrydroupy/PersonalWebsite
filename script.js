c975c95ad9002e659e4656c2d466bec81e0d2ed3
Landry Droupy <Landry.J.Droupy-1@ou.edu>
2022-04-07 13:24:28 -0500

    Need formatting

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.tabTarget)
         tabContents.forEach(tabContent => {
-            tabContent.classList.remove('active')})
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
         target.classList.add('active')
     })
 })
