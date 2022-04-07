66450089ca6075677fce49e14cba99371f4b567f
Landry Droupy <Landry.J.Droupy-1@ou.edu>
2022-04-07 11:13:27 -0500

    semi - working

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// JavaScript source code
+const tabs = document.querySelectorAll('[data-tab-target]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        target.classList.add('active')
+    })
+})
